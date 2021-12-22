40b9480be1f03a52fae7472e3e36fb91fbfc87b2
Fermin Aguilar <ferminab99@gmail.com>
2021-12-22 18:23:35 -0300

    se agrego mongo atlas y heroku

--- src/database.js
+++ src/database.js
@@ -1,8 +1,8 @@
 
 import mongoose from 'mongoose';
 
-const url = 'mongodb://localhost:27017/rollingnews';
-
+// const url = 'mongodb://localhost:27017/rollingnews';
+const url = 'mongodb+srv://grupo1:rollingnews@rollingnews.5l6ke.mongodb.net/rollingnews';
 
 mongoose.connect(url,{useNewUrlParser:true});
  
